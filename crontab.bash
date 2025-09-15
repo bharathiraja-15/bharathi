676296ed505ba93f959aca851d8a05045cf92861
bharathiraja <bharathimtech11@gmail.com>
2025-09-15 15:46:21 +0530

    added a file abcd

---
+++ crontab.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+* * * * * /bin/bash -c 'for i in {1..6}; do echo "Hello Bharathi, now time is $(date)"; sleep 10; done'
+
---
+++ script.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# using for loop to print 1 to 10
+
+for i in {1..10}
+do
+    echo "welcome bharathi $i sir"
+done
+ 
